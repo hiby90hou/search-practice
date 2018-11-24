f94e3314b385049b98d84759a17c298aecfc7cd1
darkend <hiby.90hou@qq.com>
2018-11-24 21:04:00 +1100

    add find value in hash test

--- Gemfile
+++ Gemfile
@@ -6,6 +6,6 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem "rspec"
+gem 'minitest'
 
 gem 'terminal-table'
---
+++ lib/search_cli_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require './search_cli.rb'
+
+class SearchCliTest < MiniTest::Test
+  def setup
+    @organizations = FormElement.new('../resources/organizations-test.json')
+    @hash = @organizations.get_hash_by_address
+  end
+
+  def test_find_number_in_hash
+    assert_equal true, @organizations.find_value_in_nested_hash({'id' => 101}, 101), "Cannot find number in hash"
+  end
+
+  def test_find_string_in_hash
+    assert_equal true, @organizations.find_value_in_nested_hash({'id' => '101'}, '101'), "Cannot find string in hash"
+  end
+end
